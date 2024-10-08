2d5d639d6402d8d33f70b7816b1f0844f9b886c7
kshitijPayU <kshitij.tomar@payu.in>
2024-10-08 07:48:01 +0530

    mandate route added

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ router.use('/payment',require('./paymentRouter'))
 router.use('/api/v1',require('./apiRouter'))
 router.use('/api/pg/upi', require('./upiRouter'))
 router.use("/proxy",require('./proxyRouter.js'))
+router.use("/mandate",require('./mandate/newSim.js'))
 
 module.exports = router
